ad4a5922d4edda9107c10dd190350134c258e5bb
thedazydev <thedazydev@gmail.com>
2023-02-08 14:23:21 +0000

    Add coverage

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -3,26 +3,26 @@ import Greet from "./greet"
 
 describe('Greet', () => {
 
-    test('should render hello', () => {
+    it('should render hello', () => {
         render(<Greet />)
         const textElement = screen.getByText(/hello/i)
         expect(textElement).toBeInTheDocument()
     })
 
-    test('greet renders with a name', () => {
+    it('greet renders with a name', () => {
         render(<Greet name="Jane" />)
         const textElement = screen.getByText('Hello Jane')
         expect(textElement).toBeInTheDocument()
     })
 
     describe('nested', () => {
-        test('should render h2 message', () => {
+        it('should render h2 message', () => {
             render(<Greet />)
             const textElement = screen.getByText(/I'm a React App/i)
             expect(textElement).toBeInTheDocument()
         })
 
-        test('h2 renders with a name', () => {
+        it('h2 renders with a name', () => {
             render(<Greet company='Google' />)
             const textElement = screen.getByText(`I'm a React App, Google`)
             expect(textElement).toBeInTheDocument()
--- src/components/greet/greet.tsx
+++ src/components/greet/greet.tsx
@@ -1,12 +1,9 @@
-type GreetProps = {
-    name?: string,
-    company?: string
-}
+import { GreetProps } from "./greet.types";
 
 function Greet(props: GreetProps) {
   return (
     <div>
-        <h1>Hello {props.name}</h1>
+        <h1>Hello {props.name ? props.name : "Guest"}</h1>
         <h2>I'm a React App, {props.company}</h2>
     </div>
   );
