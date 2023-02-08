74d33c1a43c3c43e4a06914ee3aba4d67f6db5ce
thedazydev <thedazydev@gmail.com>
2023-02-08 11:42:51 +0000

    Describe and nested

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,26 +1,32 @@
 import { render, screen } from "@testing-library/react"
 import Greet from "./greet"
 
-test('should render hello', () => {
-    render(<Greet />)
-    const textElement = screen.getByText(/hello/i)
-    expect(textElement).toBeInTheDocument()
-})
+describe('Greet', () => {
 
-test('greet renders with a name', () => {
-    render(<Greet name="Jane" />)
-    const textElement = screen.getByText('Hello Jane')
-    expect(textElement).toBeInTheDocument()
-})
+    test('should render hello', () => {
+        render(<Greet />)
+        const textElement = screen.getByText(/hello/i)
+        expect(textElement).toBeInTheDocument()
+    })
 
-test('should render h2 message', () => {
-    render(<Greet />)
-    const textElement = screen.getByText(/I'm a React App/i)
-    expect(textElement).toBeInTheDocument()
-})
+    test('greet renders with a name', () => {
+        render(<Greet name="Jane" />)
+        const textElement = screen.getByText('Hello Jane')
+        expect(textElement).toBeInTheDocument()
+    })
 
-test('h2 renders with a name', () => {
-    render(<Greet company='Google' />)
-    const textElement = screen.getByText(`I'm a React App, Google`)
-    expect(textElement).toBeInTheDocument()
+    describe('nested', () => {
+        test('should render h2 message', () => {
+            render(<Greet />)
+            const textElement = screen.getByText(/I'm a React App/i)
+            expect(textElement).toBeInTheDocument()
+        })
+
+        test('h2 renders with a name', () => {
+            render(<Greet company='Google' />)
+            const textElement = screen.getByText(`I'm a React App, Google`)
+            expect(textElement).toBeInTheDocument()
+        })
+    })
+
 })
